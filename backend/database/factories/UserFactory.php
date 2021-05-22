af7b9dad2f9e8edf2d45173cf7622f55edf68052
Alfredo Eka Wibowo <alfredoeka@outlook.com>
2021-05-23 02:55:52 +0700

    Fix user factory

--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -25,7 +25,7 @@ class UserFactory extends Factory
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => 'password',
             'department' => $this->faker->company,
             'title' => $this->faker->jobTitle,
             'remember_token' => Str::random(10),
